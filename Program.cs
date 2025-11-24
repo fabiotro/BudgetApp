dd61cd589326e92aaeb5eea615dbe4ce98f4331a
Fabio Trosi <fabio.trosi@phibus.com>
2025-11-24 14:25:47 +0100

    Add template budget repository

--- Program.cs
+++ Program.cs
@@ -13,6 +13,7 @@ builder.Services.AddScoped<ICampRepository<CampModel>, CampRepository<CampModel>
 builder.Services.AddScoped<ICategoryRepository<CategoryModel>, CategoryRepository<CategoryModel>>();
 builder.Services.AddScoped<IPositionTypeRepository<PositionTypeModel>, PositionTypeRepository<PositionTypeModel>>();
 builder.Services.AddScoped<ISubCategoryRepository<SubCategoryModel>, SubCategoryRepository<SubCategoryModel>>();
+builder.Services.AddScoped<ITemplateBudgetRepository<TemplateBudgetModel>, TemplateBudgetRepository<TemplateBudgetModel>>();
 
 var app = builder.Build();
 
